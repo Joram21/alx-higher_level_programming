03cc52882eb4b873bcc3b0b1abe5c51fd7a5528e
Joram21 <oloojoram@gmail.com>
2023-08-16 22:47:07 -0700

    SQL-more_queries

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,5 +1,6 @@
--- Show privileges
--- Show privileges for `user_0d_1`
-SHOW GRANTS FOR user_0d_1@localhost;
--- Show privileges for `user_0d_2`
-SHOW GRANTS FOR user_0d_2@localhost;
+-- a script that lists all privileges of the MySQL users
+-- user_0d_1 and user_0d_2 on your server (in localhost).
+REVOKE AUDIT_ABORT_EXEMPT, FIREWALL_EXEMPT, AUTHENTICATION_POLICY_ADMIN, GROUP_REPLICATION_STREAM, PASSWORDLESS_USER_ADMIN, SENSITIVE_VARIABLES_OBSERVER, TELEMETRY_LOG_ADMIN ON *.* FROM 'user_0d_1'@'localhost';
+REVOKE AUDIT_ABORT_EXEMPT, FIREWALL_EXEMPT, AUTHENTICATION_POLICY_ADMIN, GROUP_REPLICATION_STREAM, PASSWORDLESS_USER_ADMIN, SENSITIVE_VARIABLES_OBSERVER, TELEMETRY_LOG_ADMIN ON *.* FROM 'user_0d_2'@'localhost';
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
