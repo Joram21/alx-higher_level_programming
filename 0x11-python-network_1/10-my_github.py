c97c223c315165cf929ecab81047f9d182010cef
Joram21 <oloojoram@gmail.com>
2023-09-29 04:33:44 -0700

    urllib and requests task

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,17 +1,21 @@
 #!/usr/bin/python3
-"""
-    Python script that takes your GitHub credentials
-    (username and password) and uses the GitHub API to display your id
-"""
+# -*- coding: utf-8 -*-
+
+""" script that takes your GitHub credentials (username and password) \
+and uses the GitHub API to display your id"""
+
 import requests
-import sys
+from sys import argv
 
-if __name__ == '__main__':
-
-    r = requests.get('https://api.github.com/user',
-                     auth=(sys.argv[1], sys.argv[2]))
-    json = r.json()
+if __name__ == "__main__":
+    url = 'https://api.github.com/user'
     try:
-        print(json['id'])
-    except:
+        username = argv[1]
+        password = argv[2]
+
+        r = requests.get(url, auth=(username, password))
+        if r.status_code == 400:
+            print("None")
+        print(r.json().get('id'))
+    except IndexError:
         print("None")
