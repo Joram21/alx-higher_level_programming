0b0143c1e6f27080cfc09c26b1ddddd7f0d14aba
Joram21 <oloojoram@gmail.com>
2023-09-30 04:45:11 -0700

    bug fix

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,12 +1,21 @@
 #!/usr/bin/python3
-# -*- coding: utf-8 -*-
-
-"""script that fetches https://intranet.hbtn.io/status"""
+"""
+ fetches https://alx-intranet.hbtn.io/status
+"""
 
 import requests
 
-if __name__ == "__main__":
-    req = requests.get('https://intranet.hbtn.io/status')
+url = "https://alx-intranet.hbtn.io/status"
+
+try:
+    response = requests.get(url)
+    response.raise_for_status()
+
+    content = response.text
+
     print("Body response:")
-    print("\t- type: {}".format(type(req.text)))
-    print("\t- content: {}".format(req.text))
+    print("\t- type: {}".format(type(content)))
+    print("\t- content: {}".format(content))
+
+except requests.exceptions.RequestException as e:
+    print(f"Error fetching URL: {e}")
